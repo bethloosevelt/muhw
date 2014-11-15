8af3cdf84f85073218b426c11c0cf7c3c45b3ff8
Dale Wesa <dalewesa@Dales-Mac-Pro.local>
2014-11-15 03:01:50 -0600

    adding some basic js functionality, want to preload all images and iframes and hide slash show to save on lag

--- static/muhw.js
+++ static/muhw.js
@@ -1,4 +1,16 @@
-function test() {
 
-	alert(1);
+
+
+function music() {
+
+	contentDiv = $('#content');
+	contentDiv.empty();
+	contentDiv.append('<iframe width="100%" height="80%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/7702085&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>')
+}
+
+function shows() {
+	
+	contentDiv = $('#content');
+	contentDiv.empty();
+	contentDiv.append('<table style="width:100%"><tr><td>Jill</td><td>Smith</td> <td>50</td></tr><tr><td>Eve</td><td>Jackson</td> <td>94</td></t</table>');
 }
