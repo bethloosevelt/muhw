d2f78ce6b12752bf09558c88a8cd1a2eebc21469
Dale Wesa <wesa.dale@gmail.com>
2014-11-19 03:57:34 -0600

    adding more files

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from werkzeug.serving import run_simple
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
 
-@app.route("/musicplayer")
+@app.route("/new")
 def sendembed():
     embedHTML = {'embed': '<iframe width="100%" height="80%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/12006695&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'}
     return flask.jsonify(embedHTML)
